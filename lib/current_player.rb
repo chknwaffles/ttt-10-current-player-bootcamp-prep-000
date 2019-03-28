b6d9f49d70743e142bf2e31eba81eccc2f1639ba
remarkable-markup-1310 <wonsae.kim@gmail.com>
2019-03-28 13:50:57 +0000

    Automatically backed up by Learn

--- lib/current_player.rb
+++ lib/current_player.rb
@@ -1,7 +1,7 @@
 def turn_count(board)
   count = 0
   board.each do |token|
-    if token.includes?("X") || token.includes?("O")
+    if token.include?("X") || token.include?("O")
       count += 1
     end
   end
